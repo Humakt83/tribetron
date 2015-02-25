97195350c91168b41c51b99c77e7d24b0d3d5c32
Humakt83 <urlact@yahoo.com>
2015-02-25 16:57:01 +0200

    Tribetron: Use underscore to transpose robot queue

--- app/js/services/game.js
+++ app/js/services/game.js
@@ -3,20 +3,9 @@
 angular.module('Tribetron').factory('GameHandler', ['ngIterator', function(ngIterator) {
 
 	function buildRobotQueue(teams) {
-		var queue = [], teamIterators = [], queueLength = 0, queueIndex = 0
-		angular.forEach(teams, function(team) { 
-			teamIterators.push(ngIterator(team.robots))
-			queueLength += team.robots.length
-		})
-		while (queueIndex < queueLength) {
-			angular.forEach(teamIterators, function(teamIterator) {
-				if (teamIterator.hasNext()) {
-					queue.push(teamIterator.next())
-					queueIndex += 1
-				}
-			})
-		}
-		return queue
+		var bots = []
+		angular.forEach(teams, function(team) { bots.push(team.robots)})
+		return _.flatten(_.zip.apply(_, bots))
 	}
 
 	function GameState(teams) {
