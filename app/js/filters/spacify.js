f38453ee54dc3cfd0ced5eecedc87a3c1f812d82
Humakt83 <urlact@yahoo.com>
2015-03-01 06:53:36 +0200

    Tribetron: Couple text filters

---
+++ app/js/filters/spacify.js
@@ -0,0 +1,16 @@
+angular.module('Tribetron').filter('spacify', function() {
+    return function(input) {
+		var spacify = function(text) {
+			var specialCharactersToIgnore = '?:,./\%¤!%&;'
+			var spacifiedText = text.charAt(0)
+			angular.forEach(text.substr(1).split(''), function(ch) {
+				if (ch == ch.toUpperCase() && specialCharactersToIgnore.indexOf(ch) === -1)
+					spacifiedText += ' ' + ch.toLowerCase()
+				else
+					spacifiedText += ch
+			})
+			return spacifiedText
+		}
+		return (input) ? spacify(input) : ''
+    }
+})
