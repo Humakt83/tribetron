0fe3132011566f7db8c050c6ceaad8a16ed6cebd
Humakt83 <urlact@yahoo.com>
2015-03-08 12:42:36 +0200

    Tribetron: Couple more test cases

--- spec/trapspec.js
+++ spec/trapspec.js
@@ -17,4 +17,17 @@ describe('Testing traps', function() {
 	it('trap returns array of trap types', function() {
 		expect(trap.getTrapTypes().length).toBeGreaterThan(0)
 	})
+	
+	it('traps have cssName attribute', function() {
+		angular.forEach(trap.getTrapTypes(), function(trapType) {
+			expect(new trapType().cssName).toBeDefined()
+		})
+	})
+	
+	it('creates trap with type', function() {
+		var trapObj = trap.createTrap(trap.getTrapTypes()[0])
+		expect(trapObj).toBeDefined()
+		expect(trapObj.trapType).toBeDefined()
+		expect(trapObj.triggerTrap).toBeDefined()
+	})
 })
