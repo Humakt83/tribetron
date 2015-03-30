e6180751829c5da16c3609591ed87e1d64bb3317
Humakt83 <urlact@yahoo.com>
2015-03-31 01:45:08 +0300

    AI plays now in conquest

--- app/js/controllers/conquestcontroller.js
+++ app/js/controllers/conquestcontroller.js
@@ -32,22 +32,29 @@ angular.module('Tribetron').controller('ConquestController', ['$scope', '$locati
 	}
 	
 	function Conquest() {
-		function hasMovableArea(conquestPiece, thisGameTable) {
-			return 0 < $filter('filter')(_.flatten(thisGameTable), function(piece) {
+		this.hasMovableArea = function(conquestPiece) {
+			return $scope.conquest.getMovableAreas(conquestPiece).length > 0
+		}
+		this.getMovableAreas = function(conquestPiece) {
+			return $filter('filter')(_.flatten($scope.conquest.gameTable), function(piece) {
 				var distance = conquestPiece.distanceFromPiece(piece)
 				return !piece.owned && (distance < 2 || (distance < 3 && conquestPiece.isInSameLine(piece)))
-			}).length
+			})
 		}
-		this.isSelectable = function(piece) {
-			var thisGameTable = this.gameTable
-			var selectablePieces = $filter('filter')(_.flatten(thisGameTable), function(conquestPiece) {
+		this.isSelectable = function(piece, playerTurn) {
+			var selectablePieces = $filter('filter')(_.flatten($scope.conquest.gameTable), function(conquestPiece) {
 				if ($scope.selectedPiece) {
 					var distance = conquestPiece.distanceFromPiece($scope.selectedPiece)
-					return $scope.selectedPiece !== conquestPiece && !conquestPiece.owned && (distance < 2 || (distance < 3 && conquestPiece.isInSameLine($scope.selectedPiece)))
+					return !conquestPiece.owned && (distance < 2 || (distance < 3 && conquestPiece.isInSameLine($scope.selectedPiece)))
 				}
-				return conquestPiece.owned && conquestPiece.playerPiece && hasMovableArea(conquestPiece, thisGameTable)
+				return conquestPiece.owned && playerTurn === conquestPiece.playerPiece && $scope.conquest.hasMovableArea(conquestPiece)
 			})
 			return selectablePieces.indexOf(piece) > -1
+		}
+		this.getSelectablePieces = function(playerTurn) {
+			return $filter('filter')(_.flatten(this.gameTable), function(piece) {
+				return $scope.conquest.isSelectable(piece, playerTurn)
+			})
 		}
 		this.getPiecesNextToPiece = function(piece) {
 			return $filter('filter')(_.flatten(this.gameTable), function(conquestPiece) {
@@ -60,6 +67,16 @@ angular.module('Tribetron').controller('ConquestController', ['$scope', '$locati
 				if (piece.owned) {
 					piece.playerPiece = targetPiece.playerPiece
 				}
+			})
+		}
+		this.getAmountOfPieces = function(player) {
+			return $filter('filter')(_.flatten(this.gameTable), function(conquestPiece) {
+				return conquestPiece.owned && conquestPiece.playerPiece === player
+			}).length
+		}
+		this.getPiece = function(x, y) {
+			return _.find(_.flatten(this.gameTable), function(piece) {
+				return piece.xPosition == x && piece.yPosition == y
 			})
 		}
 		var gameTable = []
@@ -77,16 +94,46 @@ angular.module('Tribetron').controller('ConquestController', ['$scope', '$locati
 		gameTable[width-1][height-2] = new ConquestPiece(width-1, height-2, false, true)
 		this.gameTable = gameTable
 	}
+	
+	function playAITurn() {
+		function FromToScore(pieceFrom, pieceTo, score) {
+			this.from = pieceFrom
+			this.to = pieceTo
+			this.score = score
+		}
+		function evaluateScore(conquest) {
+			return conquest.getAmountOfPieces(false) - conquest.getAmountOfPieces(true)
+		}
+		function evaluateMove(from, to) {
+			var conquestCopy = angular.copy($scope.conquest)
+			conquestCopy.movePiece(conquestCopy.getPiece(from.xPosition, from.yPosition), conquestCopy.getPiece(to.xPosition, to.yPosition))
+			return evaluateScore(conquestCopy)
+		}
+		var selectablePieces = $scope.conquest.getSelectablePieces(false)
+		if (selectablePieces.length > 0) {
+			var fromToScoreMap = []
+			angular.forEach(selectablePieces, function(fromPiece) {
+				angular.forEach($scope.conquest.getMovableAreas(fromPiece), function(toPiece) {
+					fromToScoreMap.push(new FromToScore(fromPiece, toPiece, evaluateMove(fromPiece, toPiece)))
+				})
+			})
+			var moveWithMaxScore = _.max(fromToScoreMap, function(fromToScore) { return fromToScore.score})
+			var equalMoves = $filter('filter')(fromToScoreMap, function(fromToScore) { return fromToScore.score == moveWithMaxScore.score})
+			var moveToMake = equalMoves[Math.floor(Math.random() * equalMoves.length)]
+			$scope.conquest.movePiece(moveToMake.from, moveToMake.to)
+		}
+	}
 
 	$scope.conquest = new Conquest()
 	
 	$scope.selectPiece = function(piece) {
-		if ($scope.conquest.isSelectable(piece)) {
+		if ($scope.conquest.isSelectable(piece, true)) {
 			if (!$scope.selectedPiece) {
 				$scope.selectedPiece = piece
 			} else {
 				$scope.conquest.movePiece($scope.selectedPiece, piece)
 				$scope.selectedPiece = undefined
+				playAITurn()
 			}
 		}
 	}
