0f0079204c5845965432b5665818ddd4289c91b5
Humakt83 <urlact@yahoo.com>
2015-05-04 01:45:59 +0300

    Styling and more images for victory screen

--- app/js/controllers/victorycontroller.js
+++ app/js/controllers/victorycontroller.js
@@ -7,14 +7,15 @@ angular.module('Tribetron').controller('VictoryController', ['$scope', '$interva
 		$location.path('/')
 	}
 	
-	var botImages = ['colossusbot2', 'cratebot2', 'boxbot', 'lazorbot', 'player', 'titanbot', 'totterbot', 'multiplicatorbot', 'medicbot', 'nukabot']
+	var botImages = ['colossusbot2', 'cratebot2', 'boxbot', 'lazorbot', 'player', 'titanbot', 'totterbot', 'multiplicatorbot', 'medicbot', 'nukabot', 'doctorbot2', 'hackerbot',
+					'megahunterbot', 'robobasic2', 'tauronbot2', 'trapperbot', 'treasure', 'psychomedicbot2', 'psychobot', 'hottotbot', 'emanatorbot2', 'combinatorAbot2']
 	
 	var canvas = angular.element('.victory-canvas')[0]
 	var ctx = canvas.getContext('2d')
 	
 	$scope.imageDrawer = $interval( function() {
 		function imageLoaded() {
-			ctx.drawImage(image, Math.floor(Math.random() * canvas.width), Math.floor(Math.random() * canvas.height), 25, 25)
+			ctx.drawImage(image, Math.floor(Math.random() * canvas.width), Math.floor(Math.random() * canvas.height), 30, 30)
 		}
 		var bot = botImages[Math.floor(Math.random() * botImages.length)]
 		var image = angular.element('<img src="img/' + bot + '.png"/>')[0]
